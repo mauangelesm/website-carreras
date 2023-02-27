15f15c17b8144ebd67704743545839147fd462bd
Mauricio Angeles <75399750+mauangelesm@users.noreply.github.com>
2023-02-27 17:05:53 +0000

    Using dynamic data and templates

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Puestos = [
+  {
+   "id": 1,
+    "nombre": "Director de Escena",
+    "Lugar de trabajo": "Ensenada, BCS"
+  },
+  {
+   "id": 2,
+    "nombre": "Director de Operaciones",
+    "Lugar de trabajo": "Tijuana, BC"
+  },
+  {
+   "id": 3,
+    "nombre": "Director de Baile",
+    "Lugar de trabajo": "Saltillo, Coahulia"
+  },
+  {
+   "id": 4,
+    "nombre": "Director de Orquesta",
+    "Lugar de trabajo": "Torreon, Coahulia"
+  } 
+]
+
+
 @app.route("/")
 def hola():
-    return render_template('home.html')
+    return render_template('home.html',
+                          jobs=Puestos)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
